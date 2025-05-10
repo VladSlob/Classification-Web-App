39d1a45343a7dd2f55f7624a689ae32fc7959361
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-05-10 20:17:31 +0300

    app.py

--- app.py
+++ app.py
@@ -2,34 +2,67 @@ import os
 
 import tensorflow as tf
 from flask import Flask, request, render_template
+from classifier import classify
 
-from classifire import classify
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 STATIC_FOLDER = "static"
-UPLOAD_FOLDER = "static/uploads/"
+UPLOAD_FOLDER = "static/images"
 
-cnn_model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "save_at_49.keras")
+cnn_model = tf.keras.models.load_model(
+    STATIC_FOLDER + "/models/" + "tires_model_light.keras"
+)
+
+
+def save_uploaded_file(file) -> str:
+    uploaded_img_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(uploaded_img_path)
+    return uploaded_img_path
+
+
+def determine_result_classes(label: str) -> tuple[str, str]:
+    background_class = "road-bg"
+    message_class = ""
+
+    if "flat" in label.lower():
+        background_class = "flat-tire-bg"
+        message_class = "advice-message--danger"
+
+    elif "OK" in label:
+        background_class = "ok-tire-bg"
+        message_class = "advice-message--success"
+
+    return background_class, message_class
+
+
+def prepare_response(
+    message: str, probability: float, background_class: str, message_class: str
+) -> str:
+    return render_template(
+        "result.html",
+        message=message,
+        probability=probability,
+        background_class=background_class,
+        message_class=message_class,
+    )
 
 
 @app.route("/")
 def home():
-    img_name = "/static/images/cat-dog.jpg"
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.post("/classify")
-def upload_file():
+def classify_image():
     file = request.files["image"]
-    upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename)
-    file.save(upload_image_path)
+    uploaded_img_path = save_uploaded_file(file)
+    label, message, prob = classify(cnn_model, uploaded_img_path)
+    probability = round(prob * 100, 0)
 
-    label, prob = classify(cnn_model, upload_image_path)
+    background_class, message_class = determine_result_classes(label)
 
-    prob = round((float(prob) * 100), 2)
-
-    return render_template("result.html", label=label, probability=prob, image=upload_image_path)
+    return prepare_response(message, probability, background_class, message_class)
 
 
 if __name__ == "__main__":
